c22d9148d5b0587f0a36ccad40a0f174abeb9f7c
edKotinsky <edkotinsky@gmail.com>
2023-05-21 18:45:20 +0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+from setuptools.command.install import install
+
+
+class InstallCommand(install):
+    user_options = install.user_options + [
+        ("type=", None, None)
+    ]
+
+    def initialize_options(self):
+        install.initialize_options(self)
+        self.type = None
+
+    def finalize_options(self):
+        install.finalize_options(self)
+
+    def run(self):
+        global installation_type
+        installation_type = self.type
+        install.run(self)
+
+
+setup(
+    name="clipdis",
+    version="0.0.1",
+    license="MIT",
+    description="Tool for sharing clipboard between host and Docker container",
+    author="Gleb Zlatanov",
+    url="https://github.com/edKotinsky/clipdis",
+    entry_points={
+        "console_scripts": [
+            "clipdis_start = cb"
+        ]
+    },
+    install_requires=[
+        "pexpect >= 4.8.0",
+        "ptyprocess >= 0.7.0",
+        "pyperclip >= 1.8.0"
+    ],
+    cmdclass={
+        "install": InstallCommand
+    }
+)
