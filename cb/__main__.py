a6884f69dcc22cd9611d5717c5cfca5153eabe71
edKotinsky <edkotinsky@gmail.com>
2023-05-21 15:07:03 +0600

    Integrate docker container runner script with the clipdis; do not create an orphaned process, just wrap docker's input and output with module's stdin and stdout

--- cb/__main__.py
+++ cb/__main__.py
@@ -2,29 +2,51 @@ from argparse import ArgumentParser
 
 from .common import run
 from .clip import clipboard_tool
-from .watcher import watcher
+from .start_docker import InteractData, start
 
 
-parser = ArgumentParser()
-parser.add_argument("--directory", type=str, required=True,
-                    help="Docker volume directory")
-parser.add_argument("--logfile", type=str, default='_',
-                    help="File to write log messages")
-group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument("--id", type=str)
-group.add_argument("--name", type=str)
-
-ns, args = parser.parse_known_args()
-
-
-# Utility name is required only by container's clipboard side
-# Container id is required by the host side
 async def main() -> int:
+    parser = ArgumentParser()
+
+    clip_args = parser.add_argument_group("clip", "Clip tool args;"
+                                          " these arguments only required if"
+                                          " module is executed on a container's"
+                                          " side")
+    clip_args.add_argument("--directory", type=str,
+                           help="Docker volume directory")
+    clip_args.add_argument("--name", type=str,
+                           help="Name of a clipboard tool;"
+                           " must be specified only on the container's side")
+
+    watcher = parser.add_argument_group("watcher",
+                                        "Args that required by host's side")
+    watcher.add_argument("-d", "--datadir", type=str,
+                         help="Directory where the container will store its"
+                         "internal data")
+    watcher.add_argument("-c", "--clipdir", type=str,
+                         help="Directory where the clipboard dispatcher files"
+                         "will be stored; in this directory the docker's volume"
+                         "will be mounted")
+    watcher.add_argument("-u", "--user", type=str, help="Username")
+    watcher.add_argument("-i", "--image", type=str, help="Image name")
+    watcher.add_argument("-n", "--containername", type=str, default="workspace",
+                         help="Container name; by default: workspace")
+    watcher.add_argument("-l", "--logfile", type=str, default='_',
+                         help="File to write log messages")
+
+    ns, args = parser.parse_known_args()
+
     try:
         if ns.name:
             await clipboard_tool(ns.name, ns.directory, args)
-        elif ns.id:
-            await watcher(ns.id, ns.directory, ns.logfile)
+        else:
+            idata = InteractData(data_directory=ns.datadir,
+                                 clip_directory=ns.clipdir,
+                                 user_name=ns.user,
+                                 image=ns.image,
+                                 container_name=ns.containername,
+                                 logfile=ns.logfile)
+            await start(idata)
         return 0
     except Exception as err:
         print(f"Error: {err}")
---
+++ cb/start_docker.py
@@ -0,0 +1,129 @@
+import logging
+import pyperclip as pyc
+from os import getcwd
+from asyncio import CancelledError, sleep, create_task, gather
+from pathlib import Path
+from typing import Sequence, Awaitable, Optional
+from pexpect import spawn
+
+from .common import FileWatcher, State, eopen, check_state
+from .constants import STATEFILE, DATAFILE
+
+MODULE = "cb.watcher"
+
+
+def _configure_logger(logfile: str):
+    if logfile == '_':
+        return
+    name = "clipboard-watcher"
+    logging.basicConfig(filename=logfile,
+                        level=logging.INFO,
+                        format=f"%(asctime)s {name} %(levelname)s: %(message)s",
+                        datefmt="%Y-%m-%d %H:%M:%S")
+    logging.info("Logging initialized")
+
+
+def _copy(filename: Path) -> None:
+    try:
+        f = eopen(filename, "rt")
+        data = str(f.read())
+        pyc.copy(data)
+    except pyc.PyperclipException as err:
+        logging.error(f"Pyperclip error: {err}")
+    except RuntimeError as err:
+        logging.error(f"File error: {err}")
+        raise
+    else:
+        logging.info("Copied")
+    finally:
+        f.close()
+
+
+def _paste(datafile: Path, statefile: Path) -> None:
+    with eopen(datafile, "wt") as f:
+        data = pyc.paste()
+        f.write(data)
+    with eopen(statefile, "wt") as f:
+        f.write(State.DONE.value)
+    logging.info("Pasted")
+
+
+def _callback(statefile: Path, datafile: Path, tasks: set) -> None:
+    state = check_state(statefile)
+    if state != State.NONE:
+        logging.info(f"State changed: {state.value}")
+        if state == State.COPY:
+            _copy(datafile)
+        elif state == State.PASTE:
+            _paste(datafile, statefile)
+
+
+class InteractData:
+    datadir: Optional[str] = ""
+    clipdir: Optional[str] = ""
+    user: Optional[str] = ""
+    image: Optional[str] = ""
+    name: Optional[str] = ""
+    logfile: Optional[str] = ""
+
+    def __init__(self, data_directory: str, clip_directory: str,
+                 user_name: str, image: str, container_name: str, logfile: str):
+        self.datadir = data_directory
+        self.clipdir = clip_directory
+        self.user = user_name
+        self.image = image
+        self.name = container_name
+        self.logfile = logfile
+
+
+def _tasks_done(tasks: Sequence[Awaitable]) -> bool:
+    for task in tasks:
+        if not task.done():
+            return False
+    return True
+
+
+async def _cancel_tasks(tasks: Sequence[Awaitable]) -> None:
+    for task in tasks:
+        task.cancel()
+    while not _tasks_done(tasks):
+        await sleep(0)
+
+
+async def _interact(data: InteractData, tasks: Sequence[Awaitable]) -> None:
+    internal_data = f"{data.datadir}:/home/{data.user}/.data"
+    workdir = f"/home/{data.user}/data"
+    workdir_volume = f"{getcwd()}:{workdir}"
+
+    args = ["run", "--interactive", "--tty", "--rm",
+            "--name", data.name,
+            "--volume", internal_data,
+            "--volume", workdir_volume,
+            "--workdir", workdir,
+            data.image, "/bin/bash"]
+
+    # TODO: find a cross-platform alternative to pexpect.spawn
+    # See https://pexpect.readthedocs.io/en/stable/overview.html#windows
+    p = spawn("docker", args=args)
+    p.interact(escape_character=None)
+
+    await _cancel_tasks(tasks)
+
+
+async def start(data: InteractData) -> None:
+    _configure_logger(data.logfile)
+
+    tasks = set()
+
+    statefile = Path(data.clipdir) / STATEFILE
+    datafile = Path(data.clipdir) / DATAFILE
+
+    watcher = FileWatcher(statefile, _callback, statefile, datafile, tasks)
+    tasks.add(create_task(watcher.watch()))
+
+    tasks.add(create_task(_interact(data, tasks)))
+
+    try:
+        await gather(*tasks)
+    except CancelledError:
+        pass
